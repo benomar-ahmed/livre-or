78668ebdc453c82258eb333568db71cb0d439c01
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2022-12-05 09:53:07 +0100

    Add file index and icone github

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Inscription</a></li>
+                <li><a href="">Connexion</a></li>
+                <li><a href="">Profil</a></li>
+                <li><a href="">Livre-or</a></li>
+                <li><a href="">Commentaire</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h1>Page d'accueil du site</h1>
+    </main>
+
+    <footer>
+        <img src="Images/icon-github.png" alt="Icone du repository github">
+    </footer>
+</body>
+</html>
