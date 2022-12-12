2c72454ff84e493ec179267db4dc9c11d6f9fdc3
benomar-ahmed <ben-omar.ahmed@laplateforme.io>
2022-12-12 10:18:18 +0100

    Add file to connect databe in plesk

--- connect.php
+++ connect.php
@@ -1,7 +1,8 @@
 <?php
-/*Base de donné*/
+/*#######   Base de donnée  #######*/
 
-$mysqli = new mysqli("localhost","root","","livreor");
+// $mysqli = new mysqli("localhost","root","","livreor");
+$mysqli = new mysqli("localhost:3306","ben-omarahmed","azertyAZERTY1234","ben-omar-ahmed_livredor");
 
 
 ?>
